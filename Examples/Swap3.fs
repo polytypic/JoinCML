b3b84854ac20f7e8a6566b335409e2d4f2f748be
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-05-28 14:34:03 +0300

    Explanation.

--- Examples/Swap3.fs
+++ Examples/Swap3.fs
@@ -1,5 +1,28 @@
 namespace JoinCML.Examples
 
+// This example implements a 3-way swap as an alternative.  It is impossible to
+// express 3-way swap as an event in the original CML.
+//
+// This implementation is almost the same as with Transactional Events (TE):
+//
+//   http://www.cs.rit.edu/~mtf/research/tx-events/ICFP06/icfp06.pdf
+//
+// The key difference between the TE version and this JoinCML version is that
+// the leader in the TE implementation takes two messages from two clients
+// sequentially:
+//
+//   do { (x2, replyCh2) <- recvEvt ch
+//      ; (x3, replyCh3) <- recvEvt ch
+//
+// while the JoinCML implementation takes two messages from two clients
+// simultaneously:
+//
+//   ~~sCh <&> ~~sCh
+//
+// The reason why JoinCML allows n-way rendezvous unlike CML is that the `join`
+// (or `<&>`) combinators effectively allows joining an n-way rendezvous and a
+// 2-way rendezvous into a (n+1)-way rendezvous.
+
 open JoinCML
 
 type Swap3<'x> = Swap3 of Ch<'x * Ch<'x * 'x>>
