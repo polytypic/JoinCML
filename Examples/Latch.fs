dbd19de4d97a76a85dd08d37d52e7acad47d8e58
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-06-06 12:11:18 +0300

    Simplified.

--- Examples/Latch.fs
+++ Examples/Latch.fs
@@ -6,9 +6,10 @@ type Latch =
   val dec: Ch<unit>
   val is0: Ch<unit>
   new n as l = {dec = Ch (); is0 = Ch ()} then
+    if n < 0 then failwithf "Latch %d" n
     let rec zero () = l.is0 *<- () |>>= zero
-    and nonzero n =
-      if 0 < n then l.dec *<- () |>>= fun () -> nonzero <| n-1 else zero ()
+    and nonzero = function 0 -> zero ()
+                         | n -> l.dec *<- () |>>= fun () -> nonzero <| n-1
     nonzero n |> Async.Start
 
 [<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
