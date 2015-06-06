9f4109bf6fc01afd69245fc142c1eb37d1462311
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-06-06 08:35:10 +0300

    Use consistent ordering of arguments.

--- Examples/Unique.fs
+++ Examples/Unique.fs
@@ -2,16 +2,16 @@ namespace JoinCML.Examples
 
 open JoinCML
 
-type Unique = {queryCh: Ch<Alt<unit> * Ch<int>>}
+type Unique = {queryCh: Ch<Ch<int> * Alt<unit>>}
 
 [<CompilationRepresentation (CompilationRepresentationFlags.ModuleSuffix)>]
 module Unique =
   let create () =
     let queryCh = Ch ()
     Server.serveAny 0 queryCh
-       <| fun (nack, _) -> nack
-       <| fun (_, replyCh) i -> (replyCh *<- i, i + 1)
+       <| fun (_, nack) -> nack
+       <| fun (replyCh, _) i -> (replyCh *<- i, i + 1)
     |> Async.Start
     {queryCh = queryCh}
 
-  let unique u = u.queryCh *<+-> fun replyCh nack -> (nack, replyCh)
+  let unique u = u.queryCh *<+-> fun replyCh nack -> (replyCh, nack)
