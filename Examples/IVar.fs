0873b5dc8d7e21684dd4febb0fc51643187caaf9
Vesa Karvonen <vesa.a.j.k@gmail.com>
2015-06-08 09:24:56 +0300

    Note on IVar implementation.

--- Examples/IVar.fs
+++ Examples/IVar.fs
@@ -1,5 +1,23 @@
 namespace JoinCML.Examples
 
+// An implementation of IVar supporting exceptions using only channels.
+//
+// Compared to CML, JoinCML seems to often require more complex implementations
+// of concurrent abstractions in order to support the join operation.  In this
+// respect JoinCML is similar to the TE as described in
+//
+//   Programming Idioms for Transactional Events
+//   http://arxiv.org/pdf/1002.0936.pdf
+//
+// In the case of IVar, the complication is that we want it to be possible to
+// read a single IVar multiple times within a single synchronous operation.
+// This is not necessary with CML, due to lack of join, so a simpler approach
+// works in CML.
+//
+// In order to make multiple reads possible, we implement read operations in RPC
+// -style.  Read operations are replied to by the IVar server using asynchronous
+// message passing.
+
 open JoinCML
 
 type IVar<'x> =
